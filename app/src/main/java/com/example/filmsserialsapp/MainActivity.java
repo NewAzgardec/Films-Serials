4817fd9c0dff021d1c73867e20cf7c3892fb3a02
Masha <masha13242@gmail.com>
2019-03-28 22:37:31 +0300

    changed disign; orientation portrait

--- app/src/main/java/com/example/filmsserialsapp/MainActivity.java
+++ app/src/main/java/com/example/filmsserialsapp/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.filmsserialsapp;
 
-import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +13,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,20 +27,21 @@ public class MainActivity extends AppCompatActivity {
     Button addButton;
     Button deleteButton;
     Button sawButton;
+    Button saveButton;
+    Button loadButton;
     ListView listView;
     ArrayList<String> listItems;
     ArrayAdapter<String> adapter;
+    static final String LOG_TAG = "myLogs";
+
+    static final String FILENAME = "file";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editText = findViewById(R.id.editText);
-        addButton = findViewById(R.id.button_add);
-        deleteButton=findViewById(R.id.button_toast_delete);
-        sawButton = findViewById(R.id.button_toast_saw);
-        listView = findViewById(R.id.list_view);
+        getId();
         listItems = new ArrayList<>();
         listItems.add("Supernatural ");
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
@@ -60,6 +67,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                            openFileOutput(FILENAME, MODE_PRIVATE)));
+                    bw.write("Содержимое файла");
+                    bw.close();
+                    Log.d(LOG_TAG, "Файл записан");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+         loadButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 try {
+                     BufferedReader br = new BufferedReader(new InputStreamReader(
+                             openFileInput(FILENAME)));
+                     String str = "";
+                     while ((str = br.readLine()) != null) {
+                         Log.d(LOG_TAG, str);
+                     }
+                 } catch (FileNotFoundException e) {
+                     e.printStackTrace();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+             }
+         });
+
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -83,30 +124,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-
-    /*private static int x = 0;
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        submitPrefs();
+    public void getId(){
+        editText = findViewById(R.id.editText);
+        addButton = findViewById(R.id.button_add);
+        deleteButton = findViewById(R.id.button_toast_delete);
+        sawButton = findViewById(R.id.button_toast_saw);
+        listView = findViewById(R.id.list_view);
+        saveButton = findViewById(R.id.save_list);
+        loadButton = findViewById(R.id.load_list);
     }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        getStoredPrefs();
-    }
-    private void submitPrefs(){
-        SharedPreferences preferences = getBaseContext().getSharedPreferences("MyPrefsX", MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("InputX","My Iteration"+x);
-        editor.commit();
-        x++;
-    }
-    private void getStoredPrefs(){
-        SharedPreferences preferences = getBaseContext().getSharedPreferences("MyPrefsX", MODE_PRIVATE);
-        String s = preferences.getString("InputX", "Not set yet");
-    }*/
 }
-
