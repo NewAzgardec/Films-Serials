1604f5c2e3c69e39c076947696b7438fb75e7fb5
Masha <masha13242@gmail.com>
2019-04-01 15:33:07 +0300

    final version

--- app/src/main/java/com/example/filmsserialsapp/MainActivity.java
+++ app/src/main/java/com/example/filmsserialsapp/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.filmsserialsapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,12 +15,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,21 +23,21 @@ public class MainActivity extends AppCompatActivity {
     Button addButton;
     Button deleteButton;
     Button sawButton;
-    Button saveButton;
     Button loadButton;
     ListView listView;
     ArrayList<String> listItems;
     ArrayAdapter<String> adapter;
-    static final String LOG_TAG = "myLogs";
-    static final String FILENAME = "file";
+
+    public static final String FILMS_SERIALS = "films_serials";
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getId();
+
         listItems = new ArrayList<>();
-        listItems.add("Supernatural ");
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
         listView.setAdapter(adapter);
 
@@ -51,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (!TextUtils.isEmpty((editText.getText().toString().trim()))) {
                     listItems.add(editText.getText().toString());
+                    editText.getText().clear();
                     adapter.notifyDataSetChanged();
                 } else {
                     Toast.makeText(MainActivity.this, "Null!", Toast.LENGTH_LONG).show();
@@ -70,37 +66,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-                            openFileOutput(FILENAME, MODE_PRIVATE)));
-                    bw.write("Содержимое файла");
-                    bw.close();
-                    Log.d(LOG_TAG, "Файл записан");
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(
-                            openFileInput(FILENAME)));
-                    String str = "";
-                    while ((str = br.readLine()) != null) {
-                        Log.d(LOG_TAG, str);
-                    }
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                readFilms();
+                v.setClickable(false);
             }
         });
 
@@ -115,10 +85,9 @@ public class MainActivity extends AppCompatActivity {
                     addStrikeThrough.setPaintFlags(0);
 
                 }
-
             }
-
         });
+
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -134,13 +103,40 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void writeFilms() {
+        SharedPreferences prefs = context.getSharedPreferences(FILMS_SERIALS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.clear();
+        for (int i = 0; i < adapter.getCount(); ++i) {
+            editor.putString(String.valueOf(i), adapter.getItem(i));
+        }
+        editor.apply();
+    }
+
+    public void readFilms() {
+        SharedPreferences prefs = context.getSharedPreferences(FILMS_SERIALS, MODE_PRIVATE);
+        for (int i = 0; ; ++i) {
+            final String str = prefs.getString(String.valueOf(Integer.valueOf(i)), "");
+            if (!str.equals("")) {
+                adapter.add(str);
+            } else {
+                break;
+            }
+        }
+    }
+
     public void getId() {
         editText = findViewById(R.id.editText);
         addButton = findViewById(R.id.button_add);
         deleteButton = findViewById(R.id.button_toast_delete);
         sawButton = findViewById(R.id.button_toast_saw);
         listView = findViewById(R.id.list_view);
-        saveButton = findViewById(R.id.save_list);
         loadButton = findViewById(R.id.load_list);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        writeFilms();
     }
 }
